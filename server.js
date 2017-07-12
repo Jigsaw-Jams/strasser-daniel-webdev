2814f54c66b530e049e990f0b78c5d9d212eef29
dj <djstras963@gmail.com>
2017-07-11 22:42:46 -0400

    Heroku port addition

--- server.js
+++ server.js
@@ -11,4 +11,6 @@ app.use(express.static(__dirname + '/public'));
 
 require("./test/app");
 
-app.listen(3000);
+// Use heroku PORT if it is present in the environment.
+var port = process.env.PORT || 3000;
+app.listen(port);
