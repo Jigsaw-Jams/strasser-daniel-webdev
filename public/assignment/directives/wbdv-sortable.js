9364d361a6d1e57a4252febe0e03b5a2ed70118e
dj <djstras963@gmail.com>
2017-08-02 15:30:41 -0400

    last time

--- public/assignment/directives/wbdv-sortable.js
+++ public/assignment/directives/wbdv-sortable.js
@@ -6,22 +6,22 @@
 
     function widgetListDirective($http) {
         function linkFunction(scope, element) {
-            //negatives to confirm actions success or failure
-            var startIndex = -1;
-            var endIndex = -1;
-
-            var ul = element.find("ul");
-
-            ul.sortable({
-                start: function(event, ui) {
-                    startIndex = $(ui.item).index();
-                },
-                stop: function(event, ui) {
-                    endIndex = $(ui.item).index();
-                    console.log([startIndex, endIndex]);
-                    $http.put("/api/widget/123?start=" + startIndex + "&end=" + endIndex)
-                }
-            });
+            // //negatives to confirm actions success or failure
+            // var startIndex = -1;
+            // var endIndex = -1;
+            //
+            // var ul = element.find("ul");
+            //
+            // ul.sortable({
+            //     start: function(event, ui) {
+            //         startIndex = $(ui.item).index();
+            //     },
+            //     stop: function(event, ui) {
+            //         endIndex = $(ui.item).index();
+            //         console.log([startIndex, endIndex]);
+            //         $http.put("/api/widget/123?start=" + startIndex + "&end=" + endIndex)
+            //     }
+            // });
 
         }
 
